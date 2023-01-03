91db02de8e8a434cbc7520d91d7cf4de1f053ef2
squidspirit <an920107.squid@gmail.com>
2023-01-03 17:08:13 +0800

    learning angular

--- Test/src/app/app.component.ts
+++ Test/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Test';
+    
+    title = 'Test';
+    jsonText = '';
+
+    constructor(private http: HttpClient) {}
+    
+    onBtnClick() {
+        console.log('clicked');
+        this.http.get('/api/student').subscribe(data => {
+            this.jsonText = JSON.stringify(data);
+        });
+    }
 }
